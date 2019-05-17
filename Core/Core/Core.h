4e7ff2f23a1d6b595da36c2e419b30ce112ff0ab
Fachri Febrian <fachripaul@gmail.com>
2019-05-17 23:39:13 +0700

    first initial

---
+++ Core/Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Fachri Febrian on 17/05/2019.
+//  Copyright © 2019 fachrifaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
